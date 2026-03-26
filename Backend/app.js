e63dcdffd563068f26ba81295917abc336a21d8c
Genius4236 <mdkhizer15@gmail.com>
2026-03-26 13:24:32 +0530

    just URL

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ dbConnection();
 app.set("trust proxy", true);
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL,
+  origin: [process.env.FRONTEND_URL, "https://rahatclinic.netlify.app/",
     // "http://localhost:5173", "http://localhost:5174", "https://rahatclinic.netlify.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
