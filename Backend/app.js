7aa64160fd5448af48316980c243886d4a5ecf72
Genius4236 <mdkhizer15@gmail.com>
2026-03-29 16:11:12 +0530

    solving Cross-Origin Issues 2

--- Backend/app.js
+++ Backend/app.js
@@ -19,10 +19,10 @@ app.set("trust proxy", true);
 
 app.use(cors({
   origin: [process.env.FRONTEND_URL, "https://rahatclinic.netlify.app/",
-    "http://localhost:5173", "http://localhost:5174", "https://rahatclinic.netlify.app"
+    "http://localhost:5173", "http://localhost:5174"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
-  withCredentials: true,
+  credentials: true,
 }));
 
 app.use(cookieParser());
